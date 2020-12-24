35018510df168d6ac84e862ebace56da9c79838c
Domenic Seccareccia <dom_seccareccia@hotmail.com>
2020-12-23 19:57:07 -0500

    Updated Copyright to 2021

--- src/test/java/com/domenicseccareccia/jpegautorotate/util/ImageUtilsTest.java
+++ src/test/java/com/domenicseccareccia/jpegautorotate/util/ImageUtilsTest.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2019-2020 Domenic Seccareccia and contributors
+ * Copyright (c) 2019-2021 Domenic Seccareccia and contributors
  * <p>
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
